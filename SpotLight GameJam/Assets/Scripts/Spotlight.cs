fdfd739d9d39467f538bd231deea2d4453030194
Loïc <loowyck@gmail.com>
2025-09-26 19:00:16 +0200

    Spotlight movement

--- SpotLight GameJam/Assets/Scripts/Spotlight.cs
+++ SpotLight GameJam/Assets/Scripts/Spotlight.cs
@@ -11,13 +11,33 @@ public class Spotlight : MonoBehaviour
     private Transform _spotlightLight;
     [SerializeField]
     private Transform _spotlightCircle;
+    [SerializeField]
+    private Vector2 _spotlightRange;
+
     public void MoveSpotlight()
     {
         Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         movement = movement.normalized;
         _spotlightCircle.position += movement * _spotlightMoveSpeed * Time.deltaTime;
 
-        _spotlightObject.rotation = Quaternion.LookRotation(_spotlightCircle.position);
+        if(_spotlightCircle.position.x > _spotlightRange.x )
+        {
+            _spotlightCircle.position = new Vector3(_spotlightRange.x, _spotlightCircle.position.y, _spotlightCircle.position.z);
+        }
+        if (_spotlightCircle.position.x < -_spotlightRange.x)
+        {
+            _spotlightCircle.position = new Vector3(-_spotlightRange.x, _spotlightCircle.position.y, _spotlightCircle.position.z);
+        }
+        if (_spotlightCircle.position.z > _spotlightRange.y)
+        {
+            _spotlightCircle.position = new Vector3(_spotlightCircle.position.x, _spotlightCircle.position.y, _spotlightRange.y);
+        }
+        if (_spotlightCircle.position.z < -_spotlightRange.y)
+        {
+            _spotlightCircle.position = new Vector3(_spotlightCircle.position.x, _spotlightCircle.position.y, -_spotlightRange.y);
+        }
+
+        _spotlightObject.rotation = Quaternion.LookRotation(transform.position - _spotlightCircle.position);
     }
 
     private void OnTriggerEnter(Collider other)
