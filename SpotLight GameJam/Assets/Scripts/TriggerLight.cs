ecc1381ca8470a11cc96b0993ee2dc9e80f9ee6d
Loïc <loowyck@gmail.com>
2025-09-26 21:53:02 +0200

    mix color spotlights

--- SpotLight GameJam/Assets/Scripts/TriggerLight.cs
+++ SpotLight GameJam/Assets/Scripts/TriggerLight.cs
@@ -3,11 +3,14 @@ using UnityEngine;
 public class TriggerLight : MonoBehaviour
 {
     private GameManager _manager;
+    private Color _orgininalColor;
+
 
     private void Start()
     {
         GameObject Manager = GameObject.FindGameObjectWithTag("GameManager");
         _manager = Manager.GetComponent<GameManager>();
+        _orgininalColor = this.GetComponent<Light>().color;
     }
     private void OnTriggerEnter(Collider other)
     {
@@ -15,12 +18,22 @@ public class TriggerLight : MonoBehaviour
         {
             _manager.IsOccupied = true;
         }
+
+        if (other.CompareTag("Spotlight"))
+        {
+            this.GetComponent<Light>().color = Color.Lerp(_orgininalColor, other.GetComponent<Light>().color, .5f);
+        }
     }
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Dancer"))
         {
             _manager.IsOccupied = false;
+        }
+
+        if (other.CompareTag("Spotlight"))
+        {
+            this.GetComponent<Light>().color = _orgininalColor;
         }
     }
 }
